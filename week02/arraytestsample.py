1773d7fc62c86806ed9df7c1d9bda98932c79c75
Iznmav <andreijhosephgania@gmail.com>
2025-01-17 15:40:53 -0500

    lab02 Rock Paper Scissor

---
+++ week02/arraytestsample.py
@@ -0,0 +1,15 @@
+import random
+
+Choices = ["Rock", "Paper", "Scissors"]
+
+Math = random.choice(Choices)
+
+playerChoice = input("Rock, Paper, Scissors? ")
+computerChoice = random.choice(Choices)
+
+print(playerChoice)
+print(computerChoice)
+
+
+
+
